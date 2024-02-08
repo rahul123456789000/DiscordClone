00b3e254aa611cd1ef5cf749cd7fc27d7775faff
rahul123456789000 <sinharahul3124@gmail.com>
2024-02-08 20:29:41 +0530

    added tailwind CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '991px',
+      // => @media (min-width: 1440px) { ... }
+      'xl': '1496px',
+    },
     extend: {},
   },
   plugins: [],
